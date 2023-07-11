3a61102695c7fdee535cc313bf1082382bcb5cda
worata23 <d51865@ccollege.ac.th>
2023-07-11 11:53:07 +0700

    init 2

--- ex01.js
+++ ex01.js
@@ -1,4 +1,11 @@
-var t = setTimeout(function() {
-    console.log('test2')
-  }, 1000)
-  console.log('test1')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
