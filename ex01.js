ada7b301fd3e496f45d32ff3f942295d01d3db7b
worata23 <d51865@ccollege.ac.th>
2023-07-11 12:46:46 +0700

    oo

--- ex01.js
+++ ex01.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Worata')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listenin on port ${port}`)
+  console('it ')
 })
